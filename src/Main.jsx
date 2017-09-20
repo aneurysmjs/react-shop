57c6fa02a322668b2a0aaf5a92b65a5264924d8f
aneurysm <aneurysmjs@outlook.com>
2017-09-21 02:54:37 +0300

    feat(components): add <Footer /> component

--- src/Main.jsx
+++ src/Main.jsx
@@ -11,16 +11,20 @@ import './assets/scss/styles.scss';
 
 import Landing from './pages/Landing/Landing';
 import Movies from './pages/Movies/Movies';
+import Footer from './components/Footer/Footer';
 
 class MainComponent extends React.Component {
 
   render() {
     return (
       <Router>
-        <div>
-          <Route exact path="/" component={Landing} /> 
-          <Route path="/movies" component={Movies} /> 
-        </div>
+        <main>
+          <div>
+            <Route exact path="/" component={Landing} />
+            <Route path="/movies" component={Movies} />
+          </div>
+          <Footer />
+        </main>
       </Router>
     );
   }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <footer className="text-center mt-2">Aneurysm</footer>
+    );
+  }
+  
+}
