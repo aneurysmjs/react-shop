c4f92f9af9288ab42217639f6a91e64b6ee595b5
aneurysmjs <aneurysmjs@outlook.com>
2017-12-20 21:07:40 -0500

    style(component): indent following ESlint's rules

--- scripts/gulp/tasks/component.js
+++ scripts/gulp/tasks/component.js
@@ -2,36 +2,36 @@
 
 import 'babel-register';
 
-import gulp     from 'gulp';
-import { join } from 'path';
-import rename   from 'gulp-rename';
+import gulp from 'gulp';
+import {join} from 'path';
+import rename from 'gulp-rename';
 import template from 'gulp-template';
-import yargs    from 'yargs';
+import yargs from 'yargs';
 
-import { 
-  resolveFolderPath, 
-  capitalCase, 
-  camelToDashCase, 
-  componentPath 
+import {
+  resolveFolderPath,
+  capitalCase,
+  camelToDashCase,
+  componentPath
 } from '../../utils/utils';
 
 gulp.task('component', () => {
 
   // extract the arguments that was given in the CLI
   let name = yargs.argv.name,
-     parentPath = yargs.argv.parent || '',
-     folder = yargs.argv.folder || 'components',
-     destPath = join(resolveFolderPath(folder), parentPath, camelToDashCase(name));
-     
+    parentPath = yargs.argv.parent || '',
+    folder = yargs.argv.folder || 'components',
+    destPath = join(resolveFolderPath(folder), parentPath, camelToDashCase(name));
+
   return gulp.src(componentPath)
-     .pipe(template({
-       name,
-       upCaseName: capitalCase(name),
-       dashCaseName: camelToDashCase(name)
-     }))
-     .pipe(rename((path) => {
-       path.basename = path.basename.replace('temp', capitalCase(name));
-     }))
-     .pipe(gulp.dest(destPath));
+    .pipe(template({
+      name,
+      upCaseName: capitalCase(name),
+      dashCaseName: camelToDashCase(name)
+    }))
+    .pipe(rename((path) => {
+      path.basename = path.basename.replace('temp', capitalCase(name));
+    }))
+    .pipe(gulp.dest(destPath));
 
 });
