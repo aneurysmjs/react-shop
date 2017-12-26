cfbe5ee8ddc0b799d2fafc66aa9b40e613335c0c
aneurysmjs <aneurysmjs@outlook.com>
2017-12-25 22:20:30 -0500

    feat(Header): use NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import { string } from 'prop-types';
 
 const Header = () => (
   <div className="container">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">React Movies</a>
+      <NavLink
+        to='/'
+        className="navbar-brand">
+        React Movies
+      </NavLink>
     </nav>
   </div>
 );
