40f3834b700c0f2baea5a1b63113f04a66b3a443
aneurysmjs <aneurysmjs@outlook.com>
2018-01-04 23:51:32 -0500

    feat(store): use __REDUX_DEVTOOLS_EXTENSION__ to access Chrome's Redux extension
    pass those as the second argument to 'createStore'

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+const REDUX_DEVTOOLS = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 import reducer from './reducers';
 
-export default createStore(reducer, applyMiddleware(thunk));
+export default createStore(reducer, REDUX_DEVTOOLS, applyMiddleware(thunk));
