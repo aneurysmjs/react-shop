d0da94c84bf2fdfdc43c5e5213f8fabf26f70478
jero <jeroflat@gmail.com>
2019-04-17 00:43:40 +0300

    chore(eslint): enable flow
    use flow annotations on <Header />

--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -1,16 +1,28 @@
+// @flow strict
 import React from 'react';
 
 import './Header.scss';
 
-const Header = ({ heading, subHeading, imgUrl = '' }) => (
-  <header className="Header__masthead" style={{'background': imgUrl}}>
+type PropsType = {
+  heading: string,
+  subHeading: string,
+  imgUrl: number,
+}
+
+const Header = ({ heading, subHeading, imgUrl = '' }: PropsType) => (
+  <header
+    className="Header__masthead"
+    style={{'background': imgUrl}}
+  >
     <div className="Header__overlay" />
     <div className="container">
       <div className="row">
         <div className="col-lg-8 col-md-10 mx-auto">
           <div className="Header__page-heading">
             <h1>{ heading }</h1>
-            <span className="Header__subheading">{subHeading}</span>
+            <span className="Header__subheading">
+              {subHeading}
+            </span>
           </div>
         </div>
       </div>
