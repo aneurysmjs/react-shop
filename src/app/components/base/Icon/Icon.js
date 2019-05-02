157d90a1c74d2527ffd64b56562d85b822199d06
jero <aneurysmjs@outlook.com>
2019-05-02 20:08:56 +0300

    feat(Icon): resolve icon's path using dinamy import
    use React's setState hook to update iconPath's when the icon is resolved

--- src/app/components/base/Icon/Icon.js
+++ src/app/components/base/Icon/Icon.js
@@ -1,19 +1,37 @@
 // @flow strict
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Icon.scss';
 
 type PropsType = {
   name: string,
   selected?: string,
+  size: number | string
 };
 
-const Icon = ({ name, selected }: PropsType) => (
-  <imgx
-    alt="icon"
-    className="icon"
-    src={`assets/svg/icons/${name}${selected ? '-selected' : ''}.svg`}
-  />
-);
+const Icon = ({ name, size }: PropsType) => {
+  const [iconPath, setIconPath] = useState('');
+  (async () => {
+    // $FlowIgnore
+    const icon = await import(`@/assets/svg/icons/${name}.svg`);
+    setIconPath(icon.default);
+  })();
+
+  return (
+    <img
+      alt={`icon ${name}`}
+      className="icon"
+      style={{
+        height: `${size}px`,
+        width: `${size}px`,
+      }}
+      src={iconPath}
+    />
+  );
+};
+
+Icon.defaultProps = {
+  size: 16,
+};
 
 export default Icon;
