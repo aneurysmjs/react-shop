2a163c18c776eb71777d0301c421d1a56b876c67
jero <aneurysmjs@outlook.com>
2019-05-02 18:50:54 +0300

    refactor(Navigation): convert to stateless component

--- src/app/components/core/Navigation/Navigation.js
+++ src/app/components/core/Navigation/Navigation.js
@@ -1,55 +1,49 @@
 // @flow strict
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-type StateType = {
-  links: Array<{
-    id: string,
-    path: string,
-    name: string,
-  }>
-};
+// type StateType = {
+//   links: Array<{
+//     id: string,
+//     path: string,
+//     name: string,
+//   }>
+// };
 
-class Navigation extends Component<{}, StateType> {
+const links = [];
 
-  state = {
-    links: [],
-  };
-
-  render() {
-    const { links } = this.state;
-    return (
-      <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
-        <NavLink
-          to="/"
-          className="navbar-brand"
-        >
-          Shop
-        </NavLink>
-        <button
-          className="navbar-toggler navbar-toggler-right"
-          type="button"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            { links.length ? links.map(({ path, name, id }) => (
-              <li key={id} className="nav-item">
-                <NavLink
-                  to={`/${path}`}
-                  key={id}
-                  activeClassName="active"
-                  className="nav-link">
-                  {name}
-                </NavLink>
-              </li>
-            )) : null }
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+function Navigation() {
+  return (
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+      <NavLink
+        to="/"
+        className="navbar-brand"
+      >
+        Shop
+      </NavLink>
+      <button
+        className="navbar-toggler navbar-toggler-right"
+        type="button"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          { links.length ? links.map(({ path, name, id }) => (
+            <li key={id} className="nav-item">
+              <NavLink
+                to={`/${path}`}
+                key={id}
+                activeClassName="active"
+                className="nav-link">
+                {name}
+              </NavLink>
+            </li>
+          )) : null }
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
