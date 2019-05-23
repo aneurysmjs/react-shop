5f27a98ca039ee9965c8996f3e61de47af5c8005
jero <aneurysmjs@outlook.com>
2019-05-23 16:29:49 +0300

    test(createReducer): adjust typing

--- src/app/store/reducers/createReducer.test.js
+++ src/app/store/reducers/createReducer.test.js
@@ -1,15 +1,25 @@
 // @flow strict
+import type { Action } from 'redux';
+
 import createReducer from './createReducer';
 
 describe('createReducer', () => {
-  const initialState = [];
+
+  type User = { name: string };
+
+  type State = Array<User>;
+
+  type ActionType = {
+    ...$Exact<Action<string>>,
+    user: User
+  };
+
   const TYPE = 'SOME_TEST_TYPE';
-  // const testAction = { type: TYPE, user: { name: 'Jero' } };
 
   it('should return a reducer', () => {
-    const reducer = createReducer(initialState, {
+    const reducer = createReducer<State, ActionType>([], {
       [TYPE](state, action) {
-        return [...state, action.user];
+        return [action.user];
       }
     });    
     expect(typeof reducer).toBe('function');
