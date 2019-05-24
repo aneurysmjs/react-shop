9f74ae208247ba3bb56acf108eeb5bdcfcff6926
jero <aneurysmjs@outlook.com>
2019-05-24 11:24:00 +0300

    fix(configureStore): apply Flow's strict typing
    comment 'persistedState' logic

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,11 @@
+// @flow strict
 import { createStore, applyMiddleware, compose } from 'redux';
-import throttle from 'lodash.throttle';
+import type { Dispatch, StoreEnhancer } from 'redux';
+
+import type { State } from '@/store/types/State';
+import type { Actions } from '@/store/types/Actions';
+
+// import throttle from 'lodash.throttle';
 
 // Middleware is the suggested way to extend Redux with custom functionality.
 import middlewares from '@/store/middlewares';
@@ -7,34 +13,34 @@ import middlewares from '@/store/middlewares';
 // import all reducers
 import reducer from '@/store/reducers';
 
-import { saveState, loadState } from './localStorage';
+// import { saveState, loadState } from './localStorage';
 
 // Get the state from localStorage
-const persistedState = loadState();
+//const persistedState = loadState();
 
 const devtools =
   typeof window !== 'undefined' &&
   typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function' &&
   // when the extension is not installed, we’re using Redux compose here.
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ actionsBlacklist: [] });
-
-const composeEnhancers = devtools || compose;
+// $FlowFixMe
+const composeEnhancers: StoreEnhancer<State, Actions, Dispatch<Actions>> = devtools || compose;
 
 function configureStore() {
   
-  const store = createStore(
+  const store = createStore<{}, Actions, Dispatch<Actions>>(
     reducer,
-    persistedState,
+    // persistedState,
     composeEnhancers(applyMiddleware(...middlewares)) // the third parameter is what is called an 'enhancer'
   );
   
-  // Save the state any time the store state changes
+  /* // Save the state any time the store state changes
   store.subscribe(throttle(() => {
     // Rather than pass the whole state object, just pass an object with the key field from the state object.
     saveState({
       product: store.getState().product,
     });
-  }, 1000));
+  }, 1000)); */
 
   return store;
 }
