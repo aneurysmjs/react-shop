2f334ec18b665dfba033f75a41c714fd70b23b5f
jero <aneurysmjs@outlook.com>
2019-05-24 11:32:32 +0300

    refactor(reducers/index.js): import 'Actions' from store's types
    use 'Actions' for combineReducers's second generic parameter

--- src/app/store/reducers/index.js
+++ src/app/store/reducers/index.js
@@ -1,11 +1,14 @@
+// @flow strict
 /**
  * @module reducers
  */
 
 import { combineReducers } from 'redux';
 
+import type { Actions } from '@/store/types/Actions';
+
 import products from './products';
 
-export default combineReducers({
+export default combineReducers<{}, Actions>({
   products
 });
