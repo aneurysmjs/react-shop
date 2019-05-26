5cb78f3bf8e893be059ea24fd9191298d65313ec
jero <aneurysmjs@outlook.com>
2019-05-26 18:46:03 +0300

    feat(fetchProducts): type return value with AsyncAction

---
+++ src/app/store/actions/fetchProducts.js
@@ -0,0 +1,22 @@
+// @flow strict
+
+import { api } from '@/api';
+
+import type { AsyncAction } from '@/store/types/Actions';
+// import type { State } from '@/store/types/State';
+import type { ProductsType } from '@/store/types/ProductsType';
+
+import * as types from '../ActionTypes';
+
+export default function fetchProducts(query: string = ''): AsyncAction<ProductsType> {
+
+  return {
+    types: [
+      types.GET_PRODUCTS_REQUEST,
+      types.GET_PRODUCTS_SUCCESS,
+      types.GET_PRODUCTS_FAILURE
+    ],
+    callAPI: () => api.get<string, ProductsType>(query),
+  };
+
+}
