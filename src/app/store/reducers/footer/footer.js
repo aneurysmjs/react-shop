4c8f5ccd06699d8d030c2a205a3156ce07d00fca
jero <aneurysmjs@outlook.com>
2019-07-10 11:12:18 -0500

    style(reducers/footer): apply Eslint's formatting

--- src/app/store/reducers/footer/footer.js
+++ src/app/store/reducers/footer/footer.js
@@ -7,7 +7,9 @@ import { FOOTER_DATA } from '@/store/ActionTypes';
 import createReducer from '../createReducer';
 
 const initialState = {
-  social: [{ id: '0', icon: 'instagram', link: 'https://instagram.com' }],
+  social: [
+    { id: '0', icon: 'instagram', link: 'https://instagram.com' },
+  ],
 };
 
 export default createReducer<FooterType, FooterActionType>(initialState, {
