24399800f4d16db6e0fed6907b59e452d41dc35e
jero <aneurysmjs@outlook.com>
2019-07-25 22:45:54 -0500

    feat(sidebar): add title, onClose and overlay
    allow close the sidebar from the title or when clicking the overlay

--- src/app/components/shared/Sidebar/Sidebar.js
+++ src/app/components/shared/Sidebar/Sidebar.js
@@ -1,29 +1,51 @@
 // @flow strict
 import React, { Fragment } from 'react';
 import type { Node } from 'react';
+
+import Icon from '@/components/base/Icon/Icon';
+
 import './Sidebar.scss';
 
 type PropsType = {
   isOpen?: boolean,
   side?: string,
+  onClose: () => void,
+  title?: string,
   children: Node,
 };
 
 const Sidebar = ({
   isOpen = false,
-  side = 'left',
+  side = 'right',
+  onClose = () => {},
+  title = 'sidebar',
   children,
 }: PropsType) => (
   <Fragment>
-    <div className="sidebar__overlay" />
+    { isOpen ? (
+      <div
+        onKeyPress={() => {}}
+        role="presentation"
+        onClick={onClose}
+        className="sidebar__overlay"
+      />) : null }
     <aside
       className="sidebar"
       style={{
-        [side]: isOpen ? 0 : '-200px',
+        [side]: isOpen ? 0 : '-300px',
       }}
     >
       <header className="sidebar__header">
-        Sidebar
+        <h3 className="sidebar__title">{ title }</h3>
+        <span
+          className="sidebar__close"
+          tabIndex="-1"
+          role="button"
+          onKeyPress={() => {}}
+          onClick={onClose}
+        >
+          <Icon path="icons/close" />
+        </span>
       </header>
       <div className="sidebar__content">
         {children}
@@ -32,8 +54,8 @@ const Sidebar = ({
   </Fragment>
 );
 
-Sidebar.defaultProps = {
-  side: 'left',
-};
+// Sidebar.defaultProps = {
+//   side: 'left',
+// };
 
 export default Sidebar;
