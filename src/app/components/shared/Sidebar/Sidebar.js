a2c516e931255228795a19bdb940f902b28074a2
jero <aneurysmjs@outlook.com>
2019-07-26 20:46:10 -0500

    feat(sidebar): allow listening for 'esc' key
    close the sidebar when user press 'escape' also display the close icon in the header

--- src/app/components/shared/Sidebar/Sidebar.js
+++ src/app/components/shared/Sidebar/Sidebar.js
@@ -1,8 +1,9 @@
 // @flow strict
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import type { Node } from 'react';
 
 import Icon from '@/components/base/Icon/Icon';
+import { KEYBOARD } from '@/constants';
 
 import './Sidebar.scss';
 
@@ -20,42 +21,54 @@ const Sidebar = ({
   onClose = () => {},
   title = 'sidebar',
   children,
-}: PropsType) => (
-  <Fragment>
-    { isOpen ? (
-      <div
-        onKeyPress={() => {}}
-        role="presentation"
-        onClick={onClose}
-        className="sidebar__overlay"
-      />) : null }
-    <aside
-      className="sidebar"
-      style={{
-        [side]: isOpen ? 0 : '-300px',
-      }}
-    >
-      <header className="sidebar__header">
-        <h3 className="sidebar__title">{ title }</h3>
-        <span
-          className="sidebar__close"
-          tabIndex="-1"
-          role="button"
-          onKeyPress={() => {}}
-          onClick={onClose}
-        >
-          <Icon path="icons/close" />
-        </span>
-      </header>
-      <div className="sidebar__content">
-        {children}
-      </div>
-    </aside>
-  </Fragment>
-);
+}: PropsType) => {
+  useEffect(() => {
+    const handle = (evt: KeyboardEvent): void => {
+      if (isOpen && evt.keyCode === KEYBOARD.ESCAPE_KEY) {
+        onClose();
+      }
+    };
 
-// Sidebar.defaultProps = {
-//   side: 'left',
-// };
+    document.addEventListener('keydown', handle);
+
+    return () => {
+      document.removeEventListener('keydown', handle);
+    };
+  });
+
+  return isOpen
+    ? (
+      <Fragment>
+        <div
+          onKeyPress={() => {}}
+          role="presentation"
+          onClick={onClose}
+          className="sidebar__overlay"
+        />
+        <aside
+          className="sidebar"
+          style={{
+            [side]: isOpen ? 0 : '-300px',
+          }}
+        >
+          <header className="sidebar__header">
+            <h3 className="sidebar__title">{ title }</h3>
+            <span
+              className="sidebar__close"
+              tabIndex="-1"
+              role="button"
+              onKeyPress={() => {}}
+              onClick={onClose}
+            >
+              <Icon path="icons/close" />
+            </span>
+          </header>
+          <div className="sidebar__content">
+            {children}
+          </div>
+        </aside>
+      </Fragment>
+    ) : null;
+};
 
 export default Sidebar;
