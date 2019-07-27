0e980fbe5ad96485ccecbbb9bcb44498df959d06
jero <aneurysmjs@outlook.com>
2019-07-26 20:59:32 -0500

    feat(header): open modal
    when click cart icon, it opens the sidebar

--- src/app/components/core/Header/Header.js
+++ src/app/components/core/Header/Header.js
@@ -1,18 +1,44 @@
 // @flow strict
-import React from 'react';
+import React, { useState } from 'react';
 
 import Icon from '@/components/base/Icon/Icon';
 import Navigation from '@/components/core/Navigation/Navigation';
+import Sidebar from '@/components/shared/Sidebar/Sidebar';
 
 import './Header.scss';
 
-const Header = () => (
-  <div className="header">
-    <Navigation />
-    <span>
-      <Icon path="icons/cart" />
-    </span>
-  </div>
-);
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
+  return (
+    <div className="header">
+      <Sidebar
+        isOpen={open}
+        onClose={handleOpen}
+        title="Cart"
+      >
+        <p className="lead">
+          You have nothing, let&apos;s shop!
+        </p>
+      </Sidebar>
+      <Navigation />
+      <div className="header__user-menu">
+        <span
+          tabIndex="-1"
+          role="button"
+          onKeyPress={() => {}}
+          onClick={handleOpen}
+        >
+          <Icon
+            size="20"
+            path="icons/cart"
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
