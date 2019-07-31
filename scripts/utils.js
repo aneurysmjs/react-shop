023aa7945cf6c4d0895b597c18a1d743d97d2d62
jero <aneurysmjs@outlook.com>
2019-07-30 22:28:46 -0500

    refactor(utils): print server's compilation errors
    create info object from stats to read from it, if it has error it should print them to the console
    
    improve #18

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,26 +1,34 @@
 const chalk = require('chalk');
 
 const logMessage = (message, level = 'info') => {
+  // eslint-disable-next-line no-nested-ternary
   const color = level === 'error' ? 'red' : level === 'warning' ? 'yellow' : 'white';
+  // eslint-disable-next-line no-console
   console.log(`[${new Date().toISOString()}]`, chalk[color](message));
 };
 
-const compilerPromise = (name, compiler) => {
-  return new Promise((resolve, reject) => {
-    compiler.hooks.compile.tap(name, () => {
-      logMessage(`[${name}] Compiling `);
-    });
-    compiler.hooks.done.tap(name, (stats) => {
-      if (!stats.hasErrors()) {
-        return resolve();
-      }
-      return reject(`Failed to compile ${name}`);
-    });
+const compilerPromise = (name, compiler) => new Promise((resolve, reject) => {
+  compiler.hooks.compile.tap(name, () => {
+    logMessage(`[${name}] Compiling `);
   });
-};
+  compiler.hooks.done.tap(name, (stats) => {
+    // eslint-disable-next-line no-console
+    // `console.log('stats', stats);
 
-const findCompiler = (multiCompiler) => (compilerName) => (
-  multiCompiler.compilers.find((compiler) => compiler.name === compilerName)
+    const info = stats.toJson();
+
+    if (!stats.hasErrors()) {
+      resolve();
+    }
+    // eslint-disable-next-line no-console
+    console.error(info.errors);
+
+    return reject(new Error(`Failed to compile ${name}`));
+  });
+});
+
+const findCompiler = multiCompiler => compilerName => (
+  multiCompiler.compilers.find(compiler => compiler.name === compilerName)
 );
 
 module.exports = {
