de09c3fe29440f9437abe0ba10d7fadc4b696da6
jero <aneurysmjs@outlook.com>
2019-08-03 13:27:09 -0500

    test(lazycomponent): update what is testing for

--- src/app/components/shared/LazyComponent/LazyComponent.test.js
+++ src/app/components/shared/LazyComponent/LazyComponent.test.js
@@ -5,9 +5,13 @@ import { render } from '@testing-library/react';
 
 import LazyComponent from './LazyComponent';
 
+const Example = () => <div> Some Component </div>;
+
 describe('LazyComponent', () => {
   it('should have component\'s name as className', () => {
-    const { container } = render(<LazyComponent />);
+    const { container } = render(
+      <LazyComponent getModule={() => Promise.resolve({ default: Example })} />,
+    );
     const div = container.firstChild;
     expect(div.className).toEqual('lazy');
   });
