e649c5386b0f3fc59c28a68f131279aed82cdc1b
jero <aneurysmjs@outlook.com>
2019-08-10 21:54:03 -0500

    test(header): fix error Invariant Violation: You should not use <Header /> outside of <Router />
    To test a component (with Jest) that contains <Route />'s stuff you need to import Router in you
    test

--- src/app/components/core/Header/Header.test.js
+++ src/app/components/core/Header/Header.test.js
@@ -1,13 +1,24 @@
 // @flow strict
 import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { BrowserRouter as Router } from 'react-router-dom';
 // $FlowFixMe
 import { render } from '@testing-library/react';
 
 import Header from './Header';
 
 describe('Header', () => {
-  it('tests something', () => {
-    const { container } = render(<Header />);
+  it('tests header\'s basics', async () => {
+    let testRenderer = {};
+
+    await act(async () => {
+      testRenderer = render(
+        <Router>
+          <Header />
+        </Router>,
+      );
+    });
+    const { container } = testRenderer;
     const header = container.firstChild;
     expect(header.className).toEqual('header');
   });
