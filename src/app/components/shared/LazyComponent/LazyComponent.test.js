3d9fcba4b02f17e5b8c28b5fa3c6f6771007ded1
jero <aneurysmjs@outlook.com>
2019-08-10 21:38:01 -0500

    test(lazycomponent): use async act
    test against div's text content
    
    fix #7

--- src/app/components/shared/LazyComponent/LazyComponent.test.js
+++ src/app/components/shared/LazyComponent/LazyComponent.test.js
@@ -1,18 +1,25 @@
 // @flow strict
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 // $FlowFixMe
 import { render } from '@testing-library/react';
 
 import LazyComponent from './LazyComponent';
 
-const Example = () => <div> Some Component </div>;
+const Example = () => <div>Some Component</div>;
 
 describe('LazyComponent', () => {
-  it('should have component\'s name as className', () => {
-    const { container } = render(
-      <LazyComponent getModule={() => Promise.resolve({ default: Example })} />,
-    );
+  it('should have component\'s name as className', async () => {
+    let testRenderer = {};
+
+    await act(async () => {
+      testRenderer = render(
+        <LazyComponent getModule={() => Promise.resolve({ default: Example })} />,
+      );
+    });
+    const { container } = testRenderer;
     const div = container.firstChild;
-    expect(div).toEqual(null);
+
+    expect(div.textContent).toEqual('Some Component');
   });
 });
