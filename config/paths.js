3f08a10e6618600094cf81ec81e0ca8eb57c619a
jero <aneurysmjs@outlook.com>
2019-08-14 18:03:58 -0500

    style(config/paths): apply eslint formatting rules

--- config/paths.js
+++ config/paths.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 const paths = {
   clientBuild: resolveApp('build/client'),
