2f87233971b566c9e57fd79d01e7ed228d803001
jero <aneurysmjs@outlook.com>
2019-08-19 22:27:48 -0500

    refactor(header): move <Sidebar /> into <UserMenu />

--- src/app/components/core/Header/Header.js
+++ src/app/components/core/Header/Header.js
@@ -1,41 +1,15 @@
 // @flow strict
-import React, { useState } from 'react';
+import React from 'react';
 
 import Navigation from '@/components/core/Navigation/Navigation';
 import { UserMenu } from '@/components/core/UserMenu';
-import { useLazy } from '@/hooks/useLazy';
 
 import './Header.scss';
 
-const Header = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => setOpen(!open);
-
-  const Sidebar = useLazy(
-    () => import(/* webpackChunkName: "Sidebar" */'@/components/shared/Sidebar/Sidebar'),
-    open,
-  );
-
-  return (
-    <div className="header">
-      {Sidebar
-        ? (
-          <Sidebar
-            title="Cart"
-            side="right"
-            isOpen={open}
-            onClose={handleOpen}
-          >
-            <p className="lead">
-              You have nothing, let&apos;s shop!
-            </p>
-          </Sidebar>)
-        : null}
-      <Navigation />
-      <UserMenu onClick={handleOpen} />
-    </div>
-  );
-};
-
+const Header = () => (
+  <div className="header">
+    <Navigation />
+    <UserMenu />
+  </div>
+);
 export default Header;
--- src/app/components/core/UserMenu/UserMenu.js
+++ src/app/components/core/UserMenu/UserMenu.js
@@ -1,7 +1,8 @@
 // @flow strict
-import React from 'react';
+import React, { useState } from 'react';
 
 import Icon from '@/components/base/Icon/Icon';
+import { useLazy } from '@/hooks/useLazy';
 
 import './UserMenu.scss';
 
@@ -10,21 +11,46 @@ type PropsType = {
 };
 
 const UserMenu = ({
+  // eslint-disable-next-line no-unused-vars
   onClick = () => {},
-}: PropsType) => (
-  <div className="user-menu">
-    <span
-      tabIndex="-1"
-      role="button"
-      onKeyPress={() => {}}
-      onClick={onClick}
-    >
-      <Icon
-        size="20"
-        path="icons/cart"
-      />
-    </span>
-  </div>
-);
+}: PropsType) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
+  const Sidebar = useLazy(
+    () => import(/* webpackChunkName: "Sidebar" */'@/components/shared/Sidebar/Sidebar'),
+    open,
+  );
+
+  return (
+    <div className="user-menu">
+      {Sidebar
+        ? (
+          <Sidebar
+            title="Cart"
+            side="right"
+            isOpen={open}
+            onClose={handleOpen}
+          >
+            <p className="lead">
+              You have nothing, let&apos;s shop!
+            </p>
+          </Sidebar>)
+        : null}
+      <span
+        tabIndex="-1"
+        role="button"
+        onKeyPress={() => {}}
+        onClick={handleOpen}
+      >
+        <Icon
+          size="20"
+          path="icons/cart"
+        />
+      </span>
+    </div>
+  );
+};
 
 export default UserMenu;
