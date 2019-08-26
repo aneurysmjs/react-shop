191e49284c33d3bd2f854bb695f241ee6c54cd9b
jero <aneurysmjs@outlook.com>
2019-08-25 22:48:15 -0500

    test(__mocks__): create axios mock
    #26

--- src/app/__mocks__/axios.js
+++ src/app/__mocks__/axios.js
@@ -1,9 +1,9 @@
 /**
  * @link https://medium.com/@kilgarenone/use-jest-to-test-redux-async-action-creator-with-axios-in-a-create-react-app-app-d9c9b52eba5e
  */
-const mockAxios = jest.genMockFromModule('axios');
+const axiosMock = jest.genMockFromModule('axios');
 
 // this is the key to fix the axios.create() undefined error!
-mockAxios.create = jest.fn(() => mockAxios);
+axiosMock.create = jest.fn(() => axiosMock);
 
-export default mockAxios;
+export default axiosMock;
