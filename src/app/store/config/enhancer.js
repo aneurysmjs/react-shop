fd8c474a8fa5de86e33457423e0d0db472bc10cf
jero <aneurysmjs@outlook.com>
2019-09-22 03:29:37 +0300

    refactor(enhancer): set EnhancerType
    improve Flow's error messaging by avoiding default value when typing StoreEnhancer<...> = devtools
    || compose since gives other kind of typing error which is not very helpful
    
    improves #34, #35

--- src/app/store/config/enhancer.js
+++ src/app/store/config/enhancer.js
@@ -1,20 +1,22 @@
 // @flow strict
-import { applyMiddleware, compose } from 'redux';
-import type { StoreEnhancer } from 'redux';
+import { compose, applyMiddleware } from 'redux';
+import type { Dispatch, StoreEnhancer } from 'redux';
 
 import middlewares from '@/store/config/middlewares';
 
 import type { State } from '@/store/types/State';
 import type { Actions } from '@/store/types/Actions';
 
+type EnhancerType = StoreEnhancer<State, Actions, Dispatch<Actions>>;
+
 /* eslint-disable */
 const devtools =
   typeof window !== 'undefined' &&
   typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function' &&
-  // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ actionsBlacklist: [] });
   /* eslint-enable */
-// $FlowFixMe
-const composeEnhancers: StoreEnhancer<State, Actions> = devtools || compose;
+const composeEnhancers = devtools || compose;
+// eslint-disable-next-line no-underscore-dangle
+const enhancer: EnhancerType = composeEnhancers(applyMiddleware(...middlewares));
 
-export default composeEnhancers(applyMiddleware(...middlewares));
+export default enhancer;
