fce86ef0956cb03bad90c5f2cf8e96eabced2a37
jero <aneurysmjs@outlook.com>
2019-09-28 13:44:28 +0300

    test(fetchproducts): update test against "payload" property
    also use makeActionCreator on fetchProducts
    
    improves #35

--- src/app/store/modules/products/actions/fetchProducts.js
+++ src/app/store/modules/products/actions/fetchProducts.js
@@ -4,27 +4,28 @@ import { api } from '@/api';
 
 import { ASYNC_ACTION_TYPE } from '@/constants';
 import * as types from '@/store/ActionTypes';
-// import type { State } from '@/store/types/State';
+
+import { makeActionCreator } from '@/store/helpers/makeActionCreator';
+
 import type { AsyncAction } from '@/store/types/Actions';
 import type { ProductsType } from '@/store/types/ProductsType';
 
 import { getProducts } from '@/store/modules/products/selectors';
 
 export default function fetchProducts(query: string = ''): AsyncAction {
-  return {
-    type: ASYNC_ACTION_TYPE,
-    meta: {
-      types: [
-        types.GET_PRODUCTS_REQUEST,
-        types.GET_PRODUCTS_SUCCESS,
-        types.GET_PRODUCTS_FAILURE,
-      ],
-      callAPI: () => api.get<string, ProductsType>(query),
-      shouldCallAPI: (state) => {
-        const products = getProducts(state);
-        // $FlowFixMe - это не должно орать
-        return !products.length;
-      },
+  const meta = {
+    types: [
+      types.GET_PRODUCTS_REQUEST,
+      types.GET_PRODUCTS_SUCCESS,
+      types.GET_PRODUCTS_FAILURE,
+    ],
+    callAPI: () => api.get<string, ProductsType>(query),
+    shouldCallAPI: (state) => {
+      const products = getProducts(state);
+      // $FlowFixMe - это не должно орать
+      return !products.length;
     },
   };
+
+  return makeActionCreator(ASYNC_ACTION_TYPE)({}, meta);
 }
