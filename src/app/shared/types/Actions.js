e098a6931a3c12f8e383f11cf3f6c6c4dca55a74
jero <aneurysmjs@outlook.com>
2019-09-29 03:35:19 +0300

    refactor(types/actions): remove AsyncAction
    is not longer needed since everything is handle on meta properties
    
    improves #34

--- src/app/shared/types/Actions.js
+++ src/app/shared/types/Actions.js
@@ -2,19 +2,8 @@
 import type { CartActionType } from '@/shared/types/CartType';
 import type { FooterActionType } from '@/shared/types/FooterType';
 import type { ProductActionType } from '@/shared/types/ProductsType';
-import type { State } from '@/shared/types/State';
 
 export type Actions =
   CartActionType |
   FooterActionType |
   ProductActionType;
-
-export type AsyncAction = {
-  type: string,
-  payload?: *,
-  meta?: {
-    types: Array<string>,
-    callAPI: () => Promise<*>,
-    shouldCallAPI?: (State) => boolean,
-  }
-};
