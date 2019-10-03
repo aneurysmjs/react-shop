67762464571fa768c68bfc7e47fbb3a1e91c2a0f
jero <aneurysmjs@outlook.com>
2019-10-03 16:51:50 +0300

    chore(jest.config): allow jest handle typescript file

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const paths = require('./config/paths');
 
 module.exports = {
   verbose: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,mjs,ts,tsx}'],
   setupFiles: [
     '<rootDir>/node_modules/regenerator-runtime/runtime',
     '<rootDir>/config/polyfills.js',
@@ -10,8 +10,8 @@ module.exports = {
   // A list of paths to modules that run some code to configure
   // or set up the testing framework before each test.
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
-    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs,ts,tsx}',
   ],
   testEnvironment: 'node',
   testURL: 'http://localhost',
@@ -21,15 +21,14 @@ module.exports = {
     /**
      * @link https://github.com/vuejs-templates/webpack/issues/807
      */
-    '^@[/](.+)': '<rootDir>/src/app/$1',
-
+    '^~[/](.+)': '<rootDir>/src/app/$1',
   },
   transform: {
-    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|mjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|css|json|ts|tsx)$)': '<rootDir>/config/jest/fileTransform.js',
   },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$'],
   moduleDirectories: paths.resolveModules,
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'mjs'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'mjs', 'ts', 'tsx'],
 };
