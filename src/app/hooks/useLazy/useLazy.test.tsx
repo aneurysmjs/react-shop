adaa1c8c4d5124d552bbef75283d4f4ce9763a93
jero <aneurysmjs@outlook.com>
2019-10-04 17:12:48 +0300

    test(uselazy): fix test by removing destructuring params added by prettier
    also apply proper typings
    
    fix #37

--- src/app/hooks/useLazy/useLazy.test.tsx
+++ src/app/hooks/useLazy/useLazy.test.tsx
@@ -1,14 +1,14 @@
-/* eslint-disable */
-
 import { renderHook } from '~/shared/utils/testing/renderHook';
 
 import useLazy from './useLazy';
 
-const getModule = () => import('./Example');
+type DynamicModule = () => Promise<typeof import('./Example')>;
+
+const getModule: DynamicModule = () => import('./Example');
 
 describe('LazyComponent', () => {
   it('should render "null" at first', () => {
-    const { result } = renderHook(() => useLazy({ getModule, cond: false }));
+    const { result } = renderHook(() => useLazy(getModule));
     expect(result.current).toEqual(null);
   });
 });
