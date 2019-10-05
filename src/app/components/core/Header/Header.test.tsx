269fd8d5664e0e82c7372baba02b7aa5facc5a8b
jero <aneurysmjs@outlook.com>
2019-10-05 21:15:35 +0300

    test(header.test): apply missing typings
    cast testRenderer as RenderWithRedux

--- src/app/components/core/Header/Header.test.tsx
+++ src/app/components/core/Header/Header.test.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import renderWithRedux from '~/shared/utils/testing/renderWithRedux';
+import renderWithRedux, { RenderWithRedux } from '~/shared/utils/testing/renderWithRedux';
 
 import Header from './Header';
 
 const headerCSS = 'd-flex vw-100 justify-content-between border-bottom bg-white';
 describe('Header', () => {
   it('tests header\'s basics', async () => {
-    let testRenderer = {};
+    let testRenderer = {} as RenderWithRedux;
 
     await act(async () => {
       testRenderer = renderWithRedux(
@@ -20,7 +20,7 @@ describe('Header', () => {
       );
     });
     const { container } = testRenderer;
-    const header = container.firstChild;
+    const header = container.firstChild as HTMLElement;
 
     expect(header.className).toEqual(headerCSS);
   });
