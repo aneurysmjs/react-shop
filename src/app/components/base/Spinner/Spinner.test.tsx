d0de8e92a06422abfa304fc7e0391fd3f3565b90
jero <aneurysmjs@outlook.com>
2019-10-05 22:26:28 +0300

    test(spinner.test): apply missing typings

--- src/app/components/base/Spinner/Spinner.test.tsx
+++ src/app/components/base/Spinner/Spinner.test.tsx
@@ -4,10 +4,15 @@ import { render } from '@testing-library/react';
 
 import Spinner from './Spinner';
 
+const defaultSize = '24px';
+
 describe('Spinner', () => {
-  it('tests something', () => {
+  // eslint-disable-next-line prettier/prettier
+  it('should \'spinner\' class and height and with equal to 24px', () => {
     const { container } = render(<Spinner />);
-    const spinner = container.firstChild;
+    const spinner = container.firstChild as HTMLDivElement;
+    expect(spinner.style.height).toEqual(defaultSize);
+    expect(spinner.style.width).toEqual(defaultSize);
     expect(spinner.className).toEqual('spinner');
   });
 });
