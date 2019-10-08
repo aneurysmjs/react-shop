3a3cb480f86e7f531edc84206fcc8551254c22fb
jero <aneurysmjs@outlook.com>
2019-10-08 16:52:59 +0300

    style(products/selectors): remove eslint-disable comment

--- src/app/store/modules/products/selectors/getProducts.tsx
+++ src/app/store/modules/products/selectors/getProducts.tsx
@@ -1,4 +1,4 @@
 import { State } from '~/shared/types/State';
-import { ProductsType } from '~/shared/types/ProductsType';
+import { ProductsType } from '~/store/modules/products/types';
 // eslint-disable-next-line import/prefer-default-export
 export const getProducts = ({ products }: State): ProductsType => products;
