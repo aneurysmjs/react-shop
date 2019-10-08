1cdec277920955371e3cc12f432847e0dd8d7662
jero <aneurysmjs@outlook.com>
2019-10-08 16:42:42 +0300

    refactor(footer reducer): write reducer with switch statement

--- src/app/store/modules/footer/reducers/footer.tsx
+++ src/app/store/modules/footer/reducers/footer.tsx
@@ -2,16 +2,20 @@ import { FooterType, FooterActionType } from '~/shared/types/FooterType';
 
 import { FOOTER_DATA } from '~/store/ActionTypes';
 
-import { createReducer } from '~/store/helpers/createReducer';
-
 const initialState = {
   social: [{ id: '0', icon: 'instagram', link: 'https://instagram.com' }],
 };
 
-export default createReducer<FooterType, FooterActionType>(initialState, {
-  [FOOTER_DATA](state) {
-    return {
-      ...state,
-    };
-  },
-});
+function footerReducer(state: FooterType = initialState, action: FooterActionType): FooterType {
+  switch (action.type) {
+    case FOOTER_DATA: {
+      return {
+        ...state,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export default footerReducer;
