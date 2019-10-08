650eea2407da1fa00947d6fc8fe72d39edd97d6b
jero <aneurysmjs@outlook.com>
2019-10-08 16:49:51 +0300

    style(reducers): remove typings
    it won't be use in the future since the store will inject reducers dynamically

--- src/app/store/reducers/index.tsx
+++ src/app/store/reducers/index.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable */
-
 import { combineReducers } from 'redux';
-
-import { Actions } from '~/shared/types/Actions';
 
 import { footer } from '~/store/modules/footer/reducers';
 import { cart } from '~/store/modules/cart/reducers';
 import { products } from '~/store/modules/products/reducers';
 
-export default combineReducers<{}, Actions>({
+export default combineReducers({
   cart,
   footer,
   products,
