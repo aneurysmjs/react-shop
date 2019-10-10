805178b4b5036f1c9da24408baa117ab94e60e36
jero <aneurysmjs@outlook.com>
2019-10-10 22:24:10 +0300

    improvement(types): add types for the whole store
    improves #43

---
+++ src/app/shared/types/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+import { Reducer } from 'redux';
+import { ProductsStateType } from '~/store/modules/products/types';
+
+export const PRODUCTS_NAMESPACE_KEY = 'PRODUCTS_NAMESPACE';
+
+export const INIT_DYNO_STATE = 'INIT_DYNO_STATE';
+
+export type FullStoreShape = {
+  [INIT_DYNO_STATE]: {};
+  [PRODUCTS_NAMESPACE_KEY]: ProductsStateType;
+};
+
+export type StoreShape = Partial<FullStoreShape>;
+export type NamespaceKey = keyof StoreShape;
+export type ReducerMap = Partial<{ [k in NamespaceKey]: Reducer<FullStoreShape[k]> }>;
