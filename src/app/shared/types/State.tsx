182f8fedc176c0b1b6727705a3b666663eaff974
jero <aneurysmjs@outlook.com>
2019-10-13 22:13:41 +0300

    refactor(shared/types): remove Cart
    it has been moved to its own store module
    
    improves #34

--- src/app/shared/types/State.tsx
+++ src/app/shared/types/State.tsx
@@ -1,9 +1,7 @@
-import { CartType } from '~/shared/types/CartType';
 import { FooterType } from '~/shared/types/FooterType';
 import { ProductsType } from '~/store/modules/products/types';
 
 export type State = {
-  cart: CartType;
   footer: FooterType;
   products: ProductsType;
 };
