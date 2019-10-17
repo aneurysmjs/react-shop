250cca395fa7cba6d2309778d02b1ece1580588b
jero <aneurysmjs@outlook.com>
2019-10-17 17:07:22 +0300

    refactor(shared/actions): remove Cart and Product action types
    they're not longer been used since they're i their own module
    
    improves #43

--- src/app/shared/types/Actions.tsx
+++ src/app/shared/types/Actions.tsx
@@ -1,5 +1,3 @@
-import { CartActionType } from '~/shared/types/CartType';
 import { FooterActionType } from '~/shared/types/FooterType';
-import { ProductActionType } from '~/store/modules/products/types';
 
-export type Actions = CartActionType | FooterActionType | ProductActionType;
+export type Actions = FooterActionType;
