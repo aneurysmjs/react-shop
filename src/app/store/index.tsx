163e73b54b00dc41a0f5ad38f62c7fb15028c1e3
jero <aneurysmjs@outlook.com>
2019-10-17 23:40:58 +0300

    refactor(store/index): replace import path with alienstore
    #improves #43

--- src/app/store/index.tsx
+++ src/app/store/index.tsx
@@ -4,9 +4,9 @@ const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   if (module.hot) {
-    module.hot.accept('~/store/config/dynoStore', () =>
+    module.hot.accept('~/store/config/alienStore', () =>
       // eslint-disable-next-line implicit-arrow-linebreak, global-require
-      require('~/store/config/dynoStore').default.reloadStore(),
+      require('~/store/config/alienStore').default.reloadStore(),
     );
   }
 }
