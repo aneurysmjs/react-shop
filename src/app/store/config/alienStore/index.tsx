8ae2d1fe9fe894c1a7ac8512f872d04fc145fb17
jero <aneurysmjs@outlook.com>
2019-10-17 16:56:05 +0300

    refactor(store/config): rename dynoStore to alienStore
    that name is already been used
    
    BREAKING CHANGE: dynoStore is now alienStore
    
    improves #43

---
+++ src/app/store/config/alienStore/index.tsx
@@ -0,0 +1,6 @@
+export { createStore } from './alienStore';
+export { reloadStore } from './alienStore';
+export { injectReducers } from './alienStore';
+export { withStoreModule } from './alienStore';
+
+export { default } from './alienStore';
