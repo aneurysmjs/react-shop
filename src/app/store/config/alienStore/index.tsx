965990ca983ba4cbe40a19825992eb90d69c412b
jero <aneurysmjs@outlook.com>
2019-11-04 21:32:55 +0300

    fix(alienstore): add removeReducers on index

--- src/app/store/config/alienStore/index.tsx
+++ src/app/store/config/alienStore/index.tsx
@@ -1,7 +1,9 @@
 export { createStore } from './alienStore';
-export { reloadStore } from './alienStore';
+export { getReducerMap } from './alienStore';
 export { injectReducers } from './alienStore';
-export { withStoreModule } from './alienStore';
+export { reloadStore } from './alienStore';
+export { removeReducers } from './alienStore';
 export { useAlienModule } from './alienStore';
+export { withStoreModule } from './alienStore';
 
 export { default } from './alienStore';
