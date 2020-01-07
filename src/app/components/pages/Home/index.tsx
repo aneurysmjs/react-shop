2b633df517dbf1164abd3ece4e4c22dc622e33d8
jero <aneurysmjs@outlook.com>
2020-01-08 01:48:45 +0300

    improvement(routing): remove withAlien logic
    use withAlien on Home's index to connect to the store

--- src/app/components/pages/Home/index.tsx
+++ src/app/components/pages/Home/index.tsx
@@ -1,1 +1,9 @@
-export { default } from './Home';
+import { FunctionComponent } from 'react';
+
+import withAlien from '~/store/config/alienStore/withAlien';
+import HomeComponent from './Home';
+
+const Home: FunctionComponent = () =>
+  withAlien(HomeComponent, () => import('~/store/modules/products/products.module'));
+
+export default Home;
--- src/app/components/routing/Routing.tsx
+++ src/app/components/routing/Routing.tsx
@@ -1,20 +1,14 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { ReactElement } from 'react';
 import { Route } from 'react-router-dom';
 
 import Layout from '~/components/core/Layout';
-import withAlien from '~/store/config/alienStore/withAlien';
 
 import Home from '~/components/pages/Home';
 
 const Routing = (): ReactElement => (
   <main>
     <Layout>
-      <Route
-        exact
-        path="/"
-        component={() => withAlien(Home, () => import('~/store/modules/products/products.module'))}
-      />
+      <Route exact path="/" component={Home} />
     </Layout>
   </main>
 );
