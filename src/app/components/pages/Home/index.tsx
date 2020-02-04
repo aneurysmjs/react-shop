508c1e37281c2860204fdfe3fc3716f99ce0d567
jero <aneurysmjs@outlook.com>
2020-02-04 19:21:48 +0300

    improvement(home): use Webpack's magic comment
    use /* webpackChunkName: "ReduxModule" */ for better bundling visualization

--- src/app/components/pages/Home/index.tsx
+++ src/app/components/pages/Home/index.tsx
@@ -3,10 +3,12 @@ import { FunctionComponent } from 'react';
 import withAlien from '~/store/config/alienStore/withAlien';
 import HomeComponent from './Home';
 
-const Home: FunctionComponent = () =>
-  withAlien(HomeComponent, {
-    id: 'products',
-    getModule: () => import('~/store/modules/products/products.module'),
-  });
+type ProductsModules = Promise<typeof import('~/store/modules/products/products.module')>;
+
+const Home: FunctionComponent<{ modules: Array<ProductsModules> }> = () =>
+  withAlien(HomeComponent, [
+    (): ProductsModules =>
+      import(/* webpackChunkName: "ReduxModule" */ '~/store/modules/products/products.module'),
+  ]);
 
 export default Home;
