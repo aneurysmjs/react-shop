0632500635d244cc6177e2d5b191070f910cde6d
jero <aneurysmjs@outlook.com>
2020-02-04 18:38:42 +0300

    fix(state): type with ProductsStateType instead of Products
    #43

--- src/app/store/State.tsx
+++ src/app/store/State.tsx
@@ -1,7 +1,7 @@
 import { FooterType } from '~/shared/types/FooterType';
-import { ProductsType } from '~/store/modules/products/types';
+import { ProductsStateType } from '~/store/modules/products/types';
 
 export type State = {
   footer: FooterType;
-  products: ProductsType;
+  products: ProductsStateType;
 };
