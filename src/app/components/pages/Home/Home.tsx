1319cbb00f54559742372d5a2762ca9ab7e55fbb
jero <aneurysmjs@outlook.com>
2020-02-04 19:37:41 +0300

    improvement(home): improve typing
    #43

--- src/app/components/pages/Home/Home.tsx
+++ src/app/components/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable prettier/prettier */
 import React, { useEffect, ReactElement } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { AlienResult } from '~/store/config/alienStore/useAlien';
+import { State } from '~/store/State';
 
 import Spinner from '~/components/base/Spinner';
 import ProductCard from '~/components/common/ProductCard';
@@ -11,15 +11,24 @@ import { ProductsStateType } from '~/store/modules/products/types';
 
 import './Home.scss';
 
-type PropsType = AlienResult;
+type PropsType = {
+  modules: Array<AlienResult<State>>;
+};
 
-const Home = (props: PropsType): ReactElement => {
+const Home = ({ modules }: PropsType): ReactElement => {
   const dispatch = useDispatch();
-  const { isLoading, products, error }: ProductsStateType = useSelector(props.selectors.getProducts);
+  const [productsModule] = modules;
+
+  const { actions, selectors } = productsModule;
+
+  const { isLoading, products, error } = useSelector<State, ProductsStateType>(
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    selectors!.getProducts,
+  );
 
   useEffect(() => {
-    dispatch(props.actions.fetchProducts(`/products`));
-  }, [dispatch, props.actions]);
+    dispatch(actions.fetchProducts(`/products`));
+  }, [dispatch, actions]);
 
   return (
     <div className="home">
@@ -33,15 +42,15 @@ const Home = (props: PropsType): ReactElement => {
         ) : null}
         {!isLoading
           ? products.map(product => (
-            <div
-              // eslint-disable-next-line no-underscore-dangle
-              key={product._id}
-              className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3"
-              data-testid="product-card-item"
-            >
-              <ProductCard width="100%" product={product} />
-            </div>
-          ))
+              <div
+                // eslint-disable-next-line no-underscore-dangle
+                key={product._id}
+                className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3"
+                data-testid="product-card-item"
+              >
+                <ProductCard width="100%" product={product} />
+              </div>
+            ))
           : null}
       </div>
     </div>
