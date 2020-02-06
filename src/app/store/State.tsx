23cbfa88a858b7d1c3237fe52e8330d7b53b5f1d
jero <aneurysmjs@outlook.com>
2020-02-06 23:40:44 +0300

    refactor(store): remove helpers
    remove actionCreator
    
    #43

--- src/app/store/State.tsx
+++
@@ -1,7 +0,0 @@
-import { FooterType } from '~/shared/types/FooterType';
-import { ProductsState } from '~/store/modules/products/types';
-
-export type State = {
-  footer: FooterType;
-  products: ProductsState;
-};
