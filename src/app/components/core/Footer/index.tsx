54dd48294ca10c69dbfb1551b2ad591007392ee1
jero <aneurysmjs@outlook.com>
2020-02-10 12:35:19 +0300

    refactor(footer/index): export directly withAlien's result

--- src/app/components/core/Footer/index.tsx
+++ src/app/components/core/Footer/index.tsx
@@ -1,14 +1,12 @@
-import { FunctionComponent } from 'react';
+import { ReactElement } from 'react';
 
 import withAlien from '~/store/config/alienStore/withAlien';
 import FooterComponent from './Footer';
 
 type FooterModule = Promise<typeof import('~/store/modules/footer/footer.module')>;
 
-const Footer: FunctionComponent<{ modules: Array<FooterModule> }> = () =>
+export default (): ReactElement =>
   withAlien(FooterComponent, [
     (): FooterModule =>
       import(/* webpackChunkName: "ReduxModule" */ '~/store/modules/footer/footer.module'),
   ]);
-
-export default Footer;
