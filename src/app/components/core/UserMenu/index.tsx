5c74d8a876160850b633b9c0fbf718aaeee4994e
jero <aneurysmjs@outlook.com>
2020-02-10 15:08:45 +0300

    test(usermenu): upgrade test by using "renderFromAlien"
    #43

--- src/app/components/core/UserMenu/index.tsx
+++ src/app/components/core/UserMenu/index.tsx
@@ -1,15 +1,12 @@
-import { FunctionComponent } from 'react';
+import { ReactElement } from 'react';
 
 import withAlien from '~/store/config/alienStore/withAlien';
 import UserMenuComponent from './UserMenu';
 
 type CartModule = Promise<typeof import('~/store/modules/cart/cart.module')>;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const UserMenu: FunctionComponent<{ modules: Array<any> }> = () =>
+export default (): ReactElement =>
   withAlien(UserMenuComponent, [
     (): CartModule =>
       import(/* webpackChunkName: "ReduxModule" */ '~/store/modules/cart/cart.module'),
   ]);
-
-export default UserMenu;
