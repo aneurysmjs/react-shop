8b2ad75b814a318e520db335674fb7b8142b22ec
jero <aneurysmjs@outlook.com>
2020-11-04 21:57:45 +0300

    style(scripts/utils): add jsdoc comments/typing

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,16 @@
 const chalk = require('chalk');
+const { map, find } = require('ramda');
 
+const NO_COMPILER_NAME = 'NO_COMPILER_NAME';
+
+const hasEslintError = info => info.errors.some(({message}) => message.includes('eslint'));
+
+/**
+ * Logs a message to the console with color information.
+ * 
+ * @param {string} message message to display in the console.
+ * @param {string} [level='info'] defines the color according the type of message.
+ */
 const logMessage = (message, level = 'info') => {
   // eslint-disable-next-line no-nested-ternary
   const color = level === 'error' ? 'red' : level === 'warning' ? 'yellow' : 'white';
@@ -7,18 +18,45 @@ const logMessage = (message, level = 'info') => {
   console.log(`[${new Date().toISOString()}]`, chalk[color](message));
 };
 
-const compilerPromise = (name, compiler) => new Promise((resolve, reject) => {
+/**
+ * @typedef {import('webpack').Compiler} Compiler
+ * @typedef {import('webpack').MultiCompiler} MultiCompiler
+ */
+
+/**
+ * Executes webpack's compiler wrapped in a promise.
+ * 
+ * @param {Compiler} compiler
+ * @return {Promise<void>}
+ */
+const compilerPromise = (compiler) => new Promise((resolve, reject) => {
+
+  if (!compiler.name) {
+    throw new Error(`${NO_COMPILER_NAME}: make sure the Webpack compiler has a 'name' property`)
+  }
+
+  const { name } = compiler;
+
+  /**
+   * Called right after beforeCompile, before a new compilation is created.
+   * 
+   * @see https://webpack.js.org/api/compiler-hooks/#compile
+   */
   compiler.hooks.compile.tap(name, () => {
     logMessage(`[${name}] Compiling `);
   });
+
+   /**
+    * Executed when the compilation has completed
+    * 
+    * @see https://webpack.js.org/api/compiler-hooks/#done
+    */
   compiler.hooks.done.tap(name, (stats) => {
     const info = stats.toJson();
 
-    if (!stats.hasErrors()) {
+    if (!stats.hasErrors() || hasEslintError(info)) {
       resolve();
     }
-    // eslint-disable-next-line no-console
-    console.error('compilerPromise error: ', info.errors);
 
     return reject(new Error(`Failed to compile ${name}`));
   });
@@ -26,14 +64,29 @@ const compilerPromise = (name, compiler) => new Promise((resolve, reject) => {
 
 const COMPILER_NAMES = ['client', 'server'];
 
-const findCompiler = (multiCompiler) => (compilerName) => (
-  multiCompiler.compilers.find((compiler) => compiler.name === compilerName)
+/**
+ * Finds a compiler by its name.
+ * 
+ * @param {MultiCompiler} multiCompiler
+ * @param {Compiler[]} multiCompiler.compilers
+ * @return {(compilerName: string) => Compiler} webpack compiler
+ */
+const findCompiler = ({ compilers }) => (compilerName) => (
+  find(({ name }) => name === compilerName, compilers)
 );
 
-const makeCompilerPromise = (compilers) => (
-  compilers.map((compiler) => compilerPromise(compiler.name, compiler))
-);
+/**
+ * 
+ * @param {Compiler[]} compilers
+ * @return {Promise<void>[]}
+ */
+const makeCompilerPromise = map(compilerPromise);
 
+/**
+ * Check the presence of server side rendering flag.
+ * 
+ * @returns boolean
+ */
 const withSSR = () => process.argv.includes('--with-ssr');
 
 const SCRIPT_TYPE = withSSR() ? 'ssr' : 'client';
