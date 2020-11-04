5cf6a54a9f5b9647105b5a7df0c81812eeb34597
jero <aneurysmjs@outlook.com>
2020-11-04 19:50:46 +0300

    style(paths): add jsdoc comment

--- config/paths.js
+++ config/paths.js
@@ -1,9 +1,24 @@
+/** @module paths */
+
 const path = require('path');
 const fs = require('fs');
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
+/**
+ * @typedef {Object} Paths
+ * @property {string} appHtml
+ * @property {string} clientBuild
+ * @property {string} serverBuild
+ * @property {string} dotenv
+ * @property {string} src
+ * @property {string} srcClient
+ * @property {string} srcServer
+ * @property {string} srcApp
+ * @property {string} publicPath
+ * @property {string[]} resolveModules
+ */
 const paths = {
   appHtml: resolveApp('config/webpack-config/template.html'),
   clientBuild: resolveApp('build/client'),
@@ -16,6 +31,10 @@ const paths = {
   publicPath: '/static/',
 };
 
+/**
+ * @type {Paths}
+ */
 paths.resolveModules = [paths.srcClient, paths.srcServer, paths.srcApp, paths.src, 'node_modules'];
+
 
 module.exports = paths;
