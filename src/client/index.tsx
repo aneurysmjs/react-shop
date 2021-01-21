f886951371cae20b5a683f286578c41bb5564bfd
aneurysm <aneurysmjs@outlook.com>
2021-01-21 23:31:49 +0300

    refactor(src/client/index): use HashRouter

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createBrowserHistory } from 'history';
 import reactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import store from '~/store';
 
