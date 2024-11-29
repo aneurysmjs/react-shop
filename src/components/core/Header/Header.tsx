5a5edec24ca82c9aff0102053ae0db20429e4bab
aneurysmjs <aneurysmjs@outlook.com>
2024-11-29 13:56:07 +0100

    feat(*): enable shadcn ui

--- src/components/core/Header/Header.tsx
+++ src/components/core/Header/Header.tsx
@@ -1,11 +1,9 @@
 'use client';
 import type { FC } from 'react';
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import clsx from 'clsx';
 import { useTranslations } from 'next-intl';
 
 import { Link } from '@/i18n/routing';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { usePathname } from 'next/navigation';
 
 import { ABOUT_ROUTE } from '@/constants';
@@ -16,41 +14,6 @@ interface Navigation {
   i18nPath: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const MobileMenuButton = () => {
-  return (
-    <DisclosureButton
-      className={`
-        group relative inline-flex items-center justify-center rounded-md p-2
-        text-gray-400
-
-        focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white
-
-        hover:bg-gray-700 hover:text-white
-      `}
-    >
-      <span className="absolute -inset-0.5" />
-      <span className="sr-only">Open main menu</span>
-      <Bars3Icon
-        aria-hidden="true"
-        className={`
-          block h-6 w-6
-
-          group-data-[open]:hidden
-        `}
-      />
-      <XMarkIcon
-        aria-hidden="true"
-        className={`
-          hidden h-6 w-6
-
-          group-data-[open]:block
-        `}
-      />
-    </DisclosureButton>
-  );
-};
-
 const Logo = () => {
   return (
     <Link href="/" className="flex-shrink-0">
@@ -58,13 +21,14 @@ const Logo = () => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
+        stroke-width="1.5"
         stroke="currentColor"
         className="size-6"
       >
         <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
         />
       </svg>
     </Link>
@@ -96,7 +60,7 @@ const NavLinks: FC<NavLinksProps> = ({ navigation, pathname }) => {
               'rounded-md px-3 py-2 text-sm font-medium',
               pathname === item.href
                 ? `
-                  bg-neutral-200 text-theme
+                  bg-neutral-200
 
                   dark:bg-neutral-800
                 `
@@ -115,43 +79,6 @@ const NavLinks: FC<NavLinksProps> = ({ navigation, pathname }) => {
     </div>
   );
 };
-interface MobileNavLinksProps {
-  navigation: Navigation[];
-  pathname: string;
-}
-
-const MobileNavLinks: FC<MobileNavLinksProps> = ({ navigation, pathname }) => {
-  const t = useTranslations('components.core.header.links');
-
-  return (
-    <div className="space-y-1 px-2 pb-3 pt-2">
-      {navigation.map((item) => (
-        <DisclosureButton
-          key={item.i18nPath}
-          as={Link}
-          href={item.href}
-          className={clsx(
-            'block rounded-md px-3 py-2 text-base font-medium',
-            pathname === item.href
-              ? `
-                bg-neutral-200
-
-                dark:bg-neutral-800
-              `
-              : `
-                dark:hover:bg-neutral-800
-
-                hover:bg-neutral-200
-              `,
-          )}
-          aria-current={pathname === item.href ? 'page' : undefined}
-        >
-          {t(item.i18nPath)}
-        </DisclosureButton>
-      ))}
-    </div>
-  );
-};
 
 const Header: FC = () => {
   const pathname = usePathname();
@@ -159,10 +86,9 @@ const Header: FC = () => {
   const navigation = [{ href: ABOUT_ROUTE, i18nPath: 'about' }];
 
   return (
-    <Disclosure
-      as="nav"
+    <header
       className={`
-        fixed top-0 w-full border-b border-neutral-200 bg-theme-primary
+        fixed top-0 w-full border-b border-neutral-200
 
         dark:border-neutral-700
       `}
@@ -177,7 +103,6 @@ const Header: FC = () => {
         `}
       >
         <div className="relative flex h-16 items-center justify-between">
-          {/* <MobileMenuButton /> */}
           <div
             className={`
               flex flex-1 items-center justify-center
@@ -199,11 +124,7 @@ const Header: FC = () => {
           </div>
         </div>
       </div>
-
-      <DisclosurePanel className="sm:hidden">
-        <MobileNavLinks navigation={navigation} pathname={pathname} />
-      </DisclosurePanel>
-    </Disclosure>
+    </header>
   );
 };
 
--- src/components/core/LocaleSwitcher/LocaleSwitcherSelect.tsx
+++ src/components/core/LocaleSwitcher/LocaleSwitcherSelect.tsx
@@ -1,31 +1,30 @@
 'use client';
 
-import clsx from 'clsx';
 import { useParams } from 'next/navigation';
 import { type PropsWithChildren, type FC, useTransition, useState } from 'react';
-import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react';
 import { LanguageIcon } from '@heroicons/react/24/outline';
 
 import { type Locale, usePathname, useRouter } from '@/i18n/routing';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
 interface LocaleSwitcherSelectProps extends PropsWithChildren {
   defaultValue: string;
-  divider?: boolean;
-  label?: string;
   locales: readonly Locale[];
 }
 
-const LocaleSwitcherSelect: FC<LocaleSwitcherSelectProps> = ({
-  defaultValue,
-  divider = false,
-  label = 'Change locale',
-  locales,
-}) => {
+const LocaleSwitcherSelect: FC<LocaleSwitcherSelectProps> = ({ defaultValue, locales }) => {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
   const params = useParams();
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [selected, setSelected] = useState(() => ({ title: defaultValue }));
 
   const localeOptions = locales.map((locale) => ({
@@ -49,90 +48,24 @@ const LocaleSwitcherSelect: FC<LocaleSwitcherSelectProps> = ({
   }
 
   return (
-    <div
-      className={clsx(
-        'relative text-gray-400',
-        isPending &&
-          `
-            transition-opacity
-
-            [&:disabled]:opacity-30
-          `,
-      )}
-    >
-      <Listbox value={selected} onChange={onSelectChange}>
-        <Label className="sr-only">{label}</Label>
-        <div className="relative">
-          <ListboxButton
-            className={`
-              flex items-center justify-center
-
-              focus:outline-non
-            `}
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <LanguageIcon aria-hidden="true" className="h-5 w-5" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {localeOptions.map((option) => (
+          <DropdownMenuItem
+            disabled={isPending}
+            key={option.title}
+            onClick={() => {
+              onSelectChange(option);
+            }}
           >
-            <span className="sr-only">{label}</span>
-            <LanguageIcon aria-hidden="true" className="h-5 w-5 text-theme" />
-          </ListboxButton>
-
-          <ListboxOptions
-            transition
-            className={clsx(
-              `
-                absolute right-0 z-10 mt-2 w-44 origin-top-right overflow-hidden
-                rounded-md border-[1px] ring-1 ring-black ring-opacity-5
-                bg-theme-primary shadow-lg
-
-                dark:border-neutral-700
-
-                data-[closed]:data-[leave]:opacity-0
-
-                data-[leave]:transition data-[leave]:duration-100
-                data-[leave]:ease-in
-
-                focus:outline-none
-              `,
-              divider &&
-                `
-                  divide-y divide-neutral-200
-
-                  dark:divide-neutral-600
-                `,
-            )}
-          >
-            {localeOptions.map((option) => (
-              <ListboxOption
-                disabled={isPending}
-                key={option.title}
-                value={option}
-                className={`
-                  group cursor-pointer select-none px-4 py-2 text-sm text-theme
-
-                  dark:data-[focus]:bg-neutral-800 dark:data-[focus]:text-white
-
-                  data-[focus]:bg-neutral-100 data-[focus]:text-neutral-900
-
-                  data-[selected]:!bg-red-500
-                `}
-              >
-                <div className="flex flex-col">
-                  <div className="flex justify-between">
-                    <p
-                      className={`
-                        font-normal
-
-                        group-data-[selected]:font-semibold
-                      `}
-                    >
-                      {option.title}
-                    </p>
-                  </div>
-                </div>
-              </ListboxOption>
-            ))}
-          </ListboxOptions>
-        </div>
-      </Listbox>
-    </div>
+            {option.title}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
