059ce4d781f35a8396d3aa7d8ffcee28bd504f93
aneurysmjs <aneurysmjs@outlook.com>
2024-11-30 18:45:55 +0100

    test(ThemeSwitch): add test

--- config/vitest/setup.config.ts
+++ config/vitest/setup.config.ts
@@ -1,6 +1,20 @@
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
--- src/components/core/ThemeSwitch/ThemeSwitch.test.tsx
+++ src/components/core/ThemeSwitch/ThemeSwitch.test.tsx
@@ -1,14 +1,43 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
+import type { ReactElement, ReactNode } from 'react';
+import { render, screen, fireEvent, type RenderResult } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+import ThemeProvider from '@/providers/ThemeProvider';
 
 import ThemeSwitch from './ThemeSwitch';
 
+function renderWithThemeProvider(ui: ReactElement | ReactNode): RenderResult {
+  return {
+    ...render(
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {ui}
+      </ThemeProvider>,
+    ),
+  };
+}
+
 describe('ThemeSwitch', () => {
-  it('renders theme switch', () => {
-    render(<ThemeSwitch />);
+  it('renders theme switch with theme as "system"', () => {
+    renderWithThemeProvider(<ThemeSwitch />);
 
     const button = screen.getByRole('switch');
 
+    expect(button).not.toBeChecked();
+  });
+
+  it('toggles theme between light and dark modes', () => {
+    renderWithThemeProvider(<ThemeSwitch />);
+
+    const button = screen.getByRole('switch');
+
+    expect(button).not.toBeChecked(); // system
+
     fireEvent.click(button);
+
+    expect(button).toBeChecked(); // dark
+
+    fireEvent.click(button);
+
+    expect(button).not.toBeChecked(); // light
   });
 });
